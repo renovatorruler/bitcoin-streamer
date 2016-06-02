94fd70ba066016b7b49f71224fb8acf000dcea99
Prashant Singh Pawar <prashantpawar@gmail.com>
2016-06-01 21:19:10 -0400

    -Added bitcoin-stream route

---
+++ app/components/BitcoinStream/index.js
@@ -0,0 +1,20 @@
+/**
+*
+* BitcoinStream
+*
+*/
+
+import React from 'react';
+
+import styles from './styles.css';
+
+class BitcoinStream extends React.Component {
+  render() {
+    return (
+      <div className={ styles.bitcoinStream }>
+      </div>
+    );
+  }
+}
+
+export default BitcoinStream;
--- app/routes.js
+++ app/routes.js
@@ -34,7 +34,15 @@ export default function createRoutes() {
         importModules.catch(errorLoading);
       },
     }, {
+      path: '/bitcoin-stream',
+      getComponent(location, cb) {
+        System.import('components/BitcoinStream')
+          .then(loadModule(cb))
+          .catch(errorLoading);
+      },
+    }, {
       path: '*',
+
       name: 'notfound',
       getComponent(nextState, cb) {
         System.import('containers/NotFoundPage')
