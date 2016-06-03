7ccbf4e18fa3d8eab2d219781843deb9a62e7c68
Prashant Singh Pawar <prashantpawar@gmail.com>
2016-06-02 20:30:34 -0400

    -Added a paginated blocklist

--- app/components/BitcoinStream/index.js
+++ app/components/BitcoinStream/index.js
@@ -6,11 +6,11 @@
 
 import React from 'react';
 
+import request from 'utils/request';
 import { call } from 'redux-saga/effects';
 
 import styles from './styles.css';
 
-import request from 'utils/request';
 
 import Block from 'components/Block';
 
@@ -25,14 +25,17 @@ class BitcoinStream extends React.Component {
     const blocksUrl = "https://bitcoin.toshi.io/api/v0/blocks";
 
     request(blocksUrl)
-      .then(blocks => this.setState({blocks: blocks.data}))
+      .then(blocks => this.setState({blocks: blocks.data.slice(0, 5)}))
   }
   
   render() {
     return (
-      <ul className={ styles.bitcoinStream }>
-        { this.state.blocks.map(this.renderBlock) }
-      </ul>
+      <div>
+        <h1>Bitcoin Blockchain</h1>
+        <ul className={ styles.bitcoinStream }>
+          { this.state.blocks.map(this.renderBlock) }
+        </ul>
+      </div>
     );
   }
 
--- app/components/Block/index.js
+++ app/components/Block/index.js
@@ -6,6 +6,9 @@
 
 import React from 'react';
 
+import request from 'utils/request';
+import { call } from 'redux-saga/effects';
+
 import styles from './styles.css';
 
 class Block extends React.Component {
@@ -15,9 +18,18 @@ class Block extends React.Component {
 
   render() {
     return (
-      <div className={ styles.block }>
-      Hello
-      </div>
+      <li className={ styles.block }>
+        <h2 className={ styles.blockHash }>Block hash: {this.props.data.hash}</h2>
+        <ul className={ styles.blockDetails }>
+          {this.props.data.transaction_hashes.slice(0,10).map(this.renderTx)}
+        </ul>
+      </li>
+    );
+  }
+
+  renderTx(tx) {
+    return (
+      <li className={ styles.txHash }>{tx}</li>
     );
   }
 }
